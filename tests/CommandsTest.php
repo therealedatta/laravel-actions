62905829b9d280d30532e6a41f5b66a9f2944ba6
David <djimenez@edatta.com>
2022-10-07 04:15:25 +0200

    (tests): change CommandsTest using custom folder for base_path and app_path helpers (#2)
    Closes #1

--- tests/CommandsTest.php
+++ tests/CommandsTest.php
@@ -1,31 +1,43 @@
 <?php
 
+beforeEach(function () {
+    app()->setBasePath(implode(DIRECTORY_SEPARATOR, [__DIR__, 'TestSupport', 'base_path']));
+    app()->useAppPath(implode(DIRECTORY_SEPARATOR, [__DIR__, 'TestSupport', 'base_path', 'app']));
+});
+
+afterEach(function () {
+    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(implode(DIRECTORY_SEPARATOR, [__DIR__, 'TestSupport', 'base_path', 'app']));
+    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(implode(DIRECTORY_SEPARATOR, [__DIR__, 'TestSupport', 'base_path', 'stubs']));
+});
+
 it('make:action command class exists', function () {
     $this->assertTrue(class_exists(\Therealedatta\LaravelActions\Commands\ActionMakeCommand::class));
 });
 
 it('can call make:action command', function () {
-    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(app_path('Actions'));
+    $this->artisan('make:action MyAwesomeAction --force')->assertExitCode(0);
+    $this->assertTrue(file_exists(app_path('Actions/MyAwesomeAction.php')));
+});
+
+it('can call make:action command with custom action path', function () {
+    $this->artisan('actions:stubs --force')->assertExitCode(0);
     $this->artisan('make:action MyAwesomeAction --force')->assertExitCode(0);
     $this->assertTrue(file_exists(app_path('Actions/MyAwesomeAction.php')));
 });
 
 it('can call make:action command with subfolder', function () {
-    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(app_path('Subfolder'));
     $this->artisan('make:action Subfolder/MyAwesomeAction --force')->assertExitCode(0);
     $this->assertTrue(file_exists(app_path('Subfolder/Actions/MyAwesomeAction.php')));
 });
 
 it('can call make:action command without subfolder', function () {
     config()->set('actions.make_subfolder', null);
-    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(app_path('Subfolder'));
     $this->artisan('make:action Subfolder/MyAwesomeAction --force')->assertExitCode(0);
     $this->assertTrue(file_exists(app_path('Subfolder/MyAwesomeAction.php')));
 });
 
 it('can call make:action command with other subfolder', function () {
     config()->set('actions.make_subfolder', 'Actions2');
-    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(app_path('Subfolder'));
     $this->artisan('make:action Subfolder/MyAwesomeAction --force')->assertExitCode(0);
     $this->assertTrue(file_exists(app_path('Subfolder/Actions2/MyAwesomeAction.php')));
 });
@@ -35,7 +47,6 @@ it('actions:stubs command class exists', function () {
 });
 
 it('can call actions:stubs command', function () {
-    (new \Illuminate\Filesystem\Filesystem)->deleteDirectory(base_path('stubs'));
     $this->artisan('actions:stubs --force')->assertExitCode(0);
     $this->assertTrue(file_exists(base_path('stubs/action.stub')));
 });
